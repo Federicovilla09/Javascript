7930feab273ec93b8869f1e89f0b1667b394f432
Federico Villanueva <federubents@gmail.com>
2023-06-12 14:31:44 -0300

    Carrito de compras

--- 2-Practico/Venta de Garage/4-Fusio Proyecto/main.js
+++ 2-Practico/Venta de Garage/4-Fusio Proyecto/main.js
@@ -1,15 +1,41 @@
 const navEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamMenu = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu")
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuHamMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
   mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
+
 }
