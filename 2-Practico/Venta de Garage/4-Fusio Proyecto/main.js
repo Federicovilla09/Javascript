5e40818c4fc20f31154767bdcc3eb4fe12560a28
Federico Villanueva <federubents@gmail.com>
2023-06-13 14:29:51 -0300

    Lista de productos: HTML a partir de arrays

--- 2-Practico/Venta de Garage/4-Fusio Proyecto/main.js
+++ 2-Practico/Venta de Garage/4-Fusio Proyecto/main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const aside = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".card-container");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuHamMenu.addEventListener("click", toggleMobileMenu);
@@ -39,3 +40,77 @@ function toggleCarritoAside() {
   aside.classList.toggle("inactive");
 
 }
+
+const productList = [];
+
+productList.push(
+  {
+    nombre: "Motocicleta",
+    precio: 120,
+    imagen:
+      "https://images.pexels.com/photos/2393835/pexels-photo-2393835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  }
+);
+
+productList.push({
+  nombre: "Motocicleta",
+  precio: 120,
+  imagen:
+    "https://images.pexels.com/photos/2393835/pexels-photo-2393835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+  nombre: "Motocicleta",
+  precio: 120,
+  imagen:
+    "https://images.pexels.com/photos/2393835/pexels-photo-2393835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+  nombre: "Motocicleta",
+  precio: 120,
+  imagen:
+    "https://images.pexels.com/photos/2393835/pexels-photo-2393835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+function renderProducts(){
+
+for (product of productList) {
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
+
+  const productImg = document.createElement("img");
+  productImg.setAttribute("src", product.imagen);
+
+  const productInfo = document.createElement("div");
+  productInfo.classList.add("product-info");
+
+  const productInfoDiv = document.createElement("div");
+
+  const producPrecio = document.createElement("p");
+  producPrecio.innerText = "$" + product.precio;
+
+  const productNombre = document.createElement("p");
+  productNombre.innerText = product.nombre;
+
+  productInfoDiv.appendChild(producPrecio);
+  productInfoDiv.appendChild(productNombre);
+
+  const productInfoFigure = document.createElement("figure");
+
+  const productInfoCard = document.createElement("img");
+  productInfoCard.setAttribute("src", "../4-Fusio Proyecto/bt_add_to_cart.svg");
+
+  productInfoFigure.appendChild(productInfoCard);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
+}
+}
+
+renderProducts(productList);
